35b2953e509dab6af618e43d0f82a44cdbaf9268
khushi19018338 <khushikesarwani273@gmail.com>
2026-07-04 22:40:19 +0530

    Data-ingestion

--- src/exception.py
+++ src/exception.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     try:
         a = 1 / 0
     except Exception as e:
-        logging.info("Logging has started")
+        logging.info("we got an error")
         raise CustomException(e, sys)
