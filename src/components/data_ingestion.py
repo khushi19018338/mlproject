0d611b1937eb748306972ede00b71905683677e2
khushi19018338 <khushikesarwani273@gmail.com>
2026-07-05 19:56:22 +0530

    new hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,57 +1,95 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from dataclasses import dataclass
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
-from src.utils import save_object
+
+from src.exception import CustomException
+from src.logger import logging
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
-from src.components.model_trainer import ModelTrainerConfig
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join("artifacts", "train.csv")
-    test_data_path: str = os.path.join("artifacts", "test.csv")
-    raw_data_path: str = os.path.join("artifacts", "data.csv")
+    train_data_path = os.path.join("artifacts", "train.csv")
+    test_data_path = os.path.join("artifacts", "test.csv")
+    raw_data_path = os.path.join("artifacts", "data.csv")
+
 
 class DataIngestion:
-
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+        logging.info("Entered the data ingestion component")
+
         try:
             df = pd.read_csv("notebook/data/StudentsPerformance.csv")
-            logging.info("Read the dataset as dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True) 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logging.info("Train test split initiated")
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            logging.info("Ingestion of the data is completed")
+
+            logging.info("Dataset read successfully")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path),
+                exist_ok=True
+            )
+
+            df.to_csv(
+                self.ingestion_config.raw_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Train-Test Split Initiated")
+
+            train_set, test_set = train_test_split(
+                df,
+                test_size=0.2,
+                random_state=42
+            )
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index=False,
+                header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Data Ingestion Completed")
+
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
+
         except Exception as e:
-            obj = CustomException(e, sys)
-            raise obj
-        
+            raise CustomException(e, sys)
+
+
 if __name__ == "__main__":
+
     obj = DataIngestion()
+
     train_data, test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    train_arr, test_arr, preprocessor_path = (
-    data_transformation.initiate_data_transformation(
+
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data,
         test_data
     )
-)
+
     model_trainer = ModelTrainer()
-    r2_square = model_trainer.initiate_model_trainer(train_arr, test_arr)
-    print(f"R2 Square: {r2_square}")
+
+    r2_square = model_trainer.initiate_model_trainer(
+        train_arr,
+        test_arr
+    )
+
+    print(f"R2 Score : {r2_square}")
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,11 @@
 import os
-import pickle
 import sys
-import numpy as np
-import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
@@ -19,17 +18,38 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     try:
         report = {}
 
         for i in range(len(models)):
+
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            # Hyperparameter Tuning
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=para,
+                cv=3,
+                n_jobs=-1,
+                verbose=2
+            )
+
+            gs.fit(X_train, y_train)
+
+            # Set best parameters
+            model.set_params(**gs.best_params_)
+
+            # Train the tuned model
             model.fit(X_train, y_train)
 
+            # Prediction
             y_test_pred = model.predict(X_test)
 
+            # R2 Score
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
